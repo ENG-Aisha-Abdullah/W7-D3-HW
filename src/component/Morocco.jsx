10ed7cead99346b7ba84dd2cf17804e299c75cc7
Ayoosh <>
2025-05-22 11:36:18 +0300

    solve img

--- src/component/Morocco.jsx
+++ src/component/Morocco.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import MoroccoImage from "/src/assets/Morocco.png";
 function Morocco() {
   return (
-        <div
-      className="bg-[url('src/assets/Morocco.png')] bg-cover bg-center text-white"
-      style={{ height: '100vh' }}
+    <div
+      className="bg-cover bg-center text-white"
+      style={{
+        backgroundImage: `url(${MoroccoImage})`,
+        height: "100vh",
+      }}
     >
       <div className="flex flex-col justify-center gap-6 lg:gap-10 text-center font-extralight items-center backdrop-brightness-40 h-full px-4 ">
         <h1 className="text-bold lg:text-8xl mt-40">
-           المغرب | Morocco – شفشاون  🇲🇦
-          </h1>
+          المغرب | Morocco – شفشاون 🇲🇦
+        </h1>
         <p className="text-sm lg:text-lg max-w-2xl">
-مدينة الأزرق الصافي والهدوء الداخلي
+          مدينة الأزرق الصافي والهدوء الداخلي
         </p>
-         <p className="text-sm lg:text-lg max-w-2xl">
-
-         "في شفشاون، الأزرق لا يهدّئ العين فقط، بل يطمئن القلب"
-         </p>
-       
+        <p className="text-sm lg:text-lg max-w-2xl">
+          "في شفشاون، الأزرق لا يهدّئ العين فقط، بل يطمئن القلب"
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Morocco
+export default Morocco;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import DesertSand from '/src/assets/DesertSand.png';
+import React from "react";
+import DesertSand from "/src/assets/DesertSand.png";
 
 function Home() {
   return (
-
-
- <div
+    <div
       className="bg-[url(${DesertSand})] bg-cover bg-center text-white"
-      style={{  
+      style={{
         backgroundImage: `url(${DesertSand})`,
-        height: '100vh'}}
+        height: "100vh",
+      }}
     >
       <div className="flex flex-col justify-center gap-6 lg:gap-10 text-center font-extralight items-center backdrop-brightness-40 h-full px-4 ">
-        <h1 className="text-bold lg:text-8xl mt-40">Inesparation – أماكن من العالم</h1>
+        <h1 className="text-bold lg:text-8xl mt-40">
+          Inesparation – أماكن من العالم
+        </h1>
         <p className="text-sm lg:text-lg max-w-2xl">
-        أماكن تلهمك بجمالها، تهدّئك بسكونها، وتوقظ فيك رغبة الاكتشاف
-في كل صورة، حكاية… وفي كل مكان، شعور ينتظر أن يُلمس
+          أماكن تلهمك بجمالها، تهدّئك بسكونها، وتوقظ فيك رغبة الاكتشاف في كل
+          صورة، حكاية… وفي كل مكان، شعور ينتظر أن يُلمس
         </p>
-       
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
